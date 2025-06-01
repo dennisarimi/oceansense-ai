5dcc65b1b34a19640b506b6b21c4ac4b8ba38718
Dennis Arimi <mwirigiarimi@gmail.com>
2025-06-01 13:57:25 -0700

    Adding routing functionality

--- frontend/src/app/chat/[chatId]/page.tsx
+++ frontend/src/app/chat/[chatId]/page.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import styles from './styles.module.css'
-import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useParams, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import ChatBox from "@/components/ChatBox/chatbox";
 import ChatMessage from "@/components/ChatMessage/chatmessage";
 
 export default function ChatPage() {
     const { chatId } = useParams();
+    const searchParams = useSearchParams();
+
     const [inputValue, setInputValue] = useState('');
     const [messages, setMessages] = useState<string[]>([]);
 
+    // Read initial message from URL
+    useEffect(() => {
+        const initialMessage = sessionStorage.getItem('initialMessage');
+        if (initialMessage) {
+            setMessages([initialMessage]);
+            // sessionStorage.removeItem('initialMessage');
+        }
+    }, [searchParams]);
+    
     const handleSend = () => {
         if (inputValue.trim()) {
         setMessages((prev) => [...prev, inputValue]);
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,26 @@
 "use client"
 
+import { useRouter } from 'next/navigation';
 import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Image from 'next/image';
 import styles from './styles.module.css';
 import ChatBox from '@/components/ChatBox/chatbox';
-import ChatMessage from '@/components/ChatMessage/chatmessage';
 
 export default function LandingPage() {
   const [inputValue, setInputValue] = useState('');
   const [queries, setQueries] = useState<string[]>([]);
+  const router = useRouter();
 
   const handleSend = () => {
     if (inputValue.trim()) {
       setQueries((prev) => [...prev, inputValue]);
       setInputValue('');
+
+      // route to specific chat
+      const chatId = uuidv4(); // Generates a unique chat ID
+      sessionStorage.setItem('initialMessage', inputValue); // add user message as query param
+      router.push(`/chat/${chatId}`);
     }
   };
 
@@ -27,17 +34,6 @@ export default function LandingPage() {
           <h1>Welcome to OceanSense AI</h1>
         </div>
       </div>
-      {queries.length > 0 && (
-        <div className={styles.chatQueries}>
-          {queries.map((query, index) => (
-            <ChatMessage
-              sender="user"
-              message={query}
-              key={index}
-            />
-          ))}
-        </div>
-      )}
       <ChatBox
         inputValue={inputValue}
         setInputValue={setInputValue}
